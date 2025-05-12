dde77a2e2eac0ed7a1667e69d8b342434cebe84d
nathanielpierson <nathanielepierson@gmail.com>
2025-05-12 12:36:51 -0400

    add server js file

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require("express");
 const app = express();
 var router = express.Router();
 
+app.listen(3000);
+
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", function (req, res) {
+  res.send("Hi");
   res.render("index", { title: "Express" });
 });
 
